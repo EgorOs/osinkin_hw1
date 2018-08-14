e75f69ca195b5f60005cf4a3958a11fd0440125d
Egor Osinkin <egoros97@yandex.ru>
2018-08-14 22:12:36 +0300

    fixed errors

--- task_3.py
+++ task_3.py
@@ -6,28 +6,18 @@ while True:
     if usr_input == "cancel":
         break
     else:
-        digit = ''
-        digits_lst = []
-        for i in range(len(usr_input)-1):
-            # Using sliding two-characters frame to switch between cases
-            frame = (usr_input[i], usr_input[i+1])
-
-            if i == len(usr_input)-2: # If last frame
-                # Remove non-digit and non-minus-sign in frame[0] if exists
-                digit += frame[0] + frame[1] if frame[0].isdigit() or frame[0] == '-' else frame[1]
-                digits_lst.append(digit)
+        digits_lst = [0]
+        characters = list(usr_input)
+        prev_char = None
+        while characters:
+            ch = characters.pop(0)
+            if ch.isdigit():
+                sign = -1 if prev_char == '-' else 1
+                digits_lst[-1] = digits_lst[-1]*10 + sign * int(ch)
             else:
-                if frame[0] == '-' and frame[1].isdigit():
-                    # case: minus before digit
-                    digit += '-'
-                elif frame[0].isdigit() and frame[1].isdigit():
-                    # case: sequence of digits
-                    digit += frame[0]
-                elif frame[0].isdigit() and not frame[1].isdigit():
-                    # case: sequence of digits is broken
-                    digit += frame[0]
-                    digits_lst.append(digit)
-                    digit = ''
+                if digits_lst[-1] != 0:
+                    digits_lst.append(0)
+                prev_char = ch
+        result = sum(digits_lst)
+        print(result)
 
-        res = sum([int(i) for i in digits_lst])
-        print(res)
